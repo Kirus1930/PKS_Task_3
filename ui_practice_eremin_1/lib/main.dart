c976fd15e2bf28a99277f55f40cba3237eeb0267
Kirus1930 <k_eryomin00@mail.ru>
2025-09-19 13:11:16 +0300

    recreate project

---
+++ ui_practice_eremin_1/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Практика 3',
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void _Counter() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Практика 3'), centerTitle: true),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Flutter - кроссплатформенный фреймворк для разработки приложений на языке Dart.',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: _Counter,
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              child: Text('Push me'),
+            ),
+            SizedBox(height: 20),
+
+            Container(
+              color: Color(0xFFFFD700),
+              width: 150,
+              height: 150,
+              child: Center(
+                child: Text('$count', style: TextStyle(fontSize: 40)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
